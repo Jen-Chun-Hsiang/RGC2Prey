321ce512054542d367a6f0a5f9350be6a09c771a
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-10-31 14:58:59 -0500

    update

--- image_processing.py
+++ image_processing.py
@@ -4,5 +4,10 @@ if __name__ == "__main__":
     input_folder = "/storage1/fs1/KerschensteinerD/Active/Emily/PreyCaptureRGC/Simulation/Images" \
                    "/grass/"
     output_folder = "/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images" \
-                   "original/grass/"
+                   "/original/grass/"
     convert_images_to_png(input_folder, output_folder)
+
+    input_folder = output_folder
+    output_folder = "/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images" \
+                   "/cropped/grass/"
+    process_and_crop_images(input_folder, output_folder)
