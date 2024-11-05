c18d27b7432d201aaaf1c75613698b61e31562bc
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-11-05 14:36:32 -0600

    commit

--- image_processing.py
+++ image_processing.py
@@ -1,14 +1,12 @@
 from utils.preprocessing import convert_images_to_png, process_and_crop_images
 
 if __name__ == "__main__":
-    folder_name = 'grass'
+    folder_name = 'cricket'
     input_folder = "/storage1/fs1/KerschensteinerD/Active/Emily/PreyCaptureRGC/Simulation/Images" \
                    f"/{folder_name}/"
+    output_folder = "/storage1/fs1/KerschensteinerD/Active/Emily/PreyCaptureRGC/Simulation/Images" \
+                    "/converted/{folder_name}/"
+    convert_images_to_png(input_folder, output_folder)
     # output_folder = "/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images" \
-    #               "/original/grass/"
-    # convert_images_to_png(input_folder, output_folder)
-
-    # input_folder = output_folder
-    output_folder = "/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images" \
-                    f"/cropped/{folder_name}/"
-    process_and_crop_images(input_folder, output_folder)
+    #                 f"/cropped/{folder_name}/"
+    # process_and_crop_images(input_folder, output_folder)
--- test_cricket_image_generator.py
+++ test_cricket_image_generator.py
@@ -1,7 +1,13 @@
+import os
 import numpy as np 
+from datasets.sim_cricket import overlay_images_with_jitter_and_scaling
 
-print('Hello world')
-print('more stuff')
-print('Get more')
-print('Where is the end?')
-print('Will it synchronize?')
+rgc_canvas_size = np.array([640, 480])  # in micro
+crop_extension_fac = 1.5  # 1.5 times larger than the target area, to be convolved with proper RGC RF
+pixel_in_um = 4.375  # [task] make sure all recordings have the same value; if not, normalization is required
+angular_in_um = 31.5
+cricket_size_range = np.array([5, 20])  # visual angle (~ 20 cm for 1.5~2 cm cricket, ~ 5 cm for contact)
+
+# calculation to pixel domain
+cricket_size_range = cricket_size_range * angular_in_um / pixel_in_um
+crop_size = rgc_canvas_size * crop_extension_fac / pixel_in_um
