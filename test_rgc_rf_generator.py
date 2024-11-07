ee5122558b3098093a1f5a7b53d0bf354a55b4fc
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-11-07 12:05:19 -0600

    make a video

--- test_rgc_rf_generator.py
+++ test_rgc_rf_generator.py
@@ -1,6 +1,11 @@
 
 import torch
+import os
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
 from datasets.rgc_rf import create_hexagonal_centers, precompute_grid_centers, get_closest_indices, map_to_fixed_grid_closest
 from datasets.rgc_rf import compute_distance_decay_matrix, map_to_fixed_grid_decay
 from utils.utils import plot_position_and_save, plot_map_and_save
@@ -9,6 +14,7 @@ from utils.utils import plot_position_and_save, plot_map_and_save
 if __name__ == "__main__":
     task_id = 2
     plot_save_folder  = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Figures/'
+    video_save_folder  = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Videos/'
     file_name = 'rgc_rf_position_plot.png'
     xlim = (-120, 120)
     ylim = (-90, 90)
@@ -47,9 +53,44 @@ if __name__ == "__main__":
         plot_map_and_save(grid_values, plot_save_folder, file_name=file_name)
 
     elif task_id == 2:
+        # Parameters for video
+        video_id = 110701
+        frame_width, frame_height = 640, 480  # Example resolution
+        fps = 5  # Frames per second
+        min_video_value, max_video_value = 0, 4  # Value range for the color map
+        os.makedirs(video_save_folder, exist_ok=True)
+        output_filename = os.path.join(video_save_folder, f'RGC_proj_map_{grid_generate_method}_{video_id}.mp4')
+        # Initialize OpenCV video writer
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        video_writer = cv2.VideoWriter(output_filename, fourcc, fps, (frame_width, frame_height))
+
         for i in range(num_step):
             values = np.random.uniform(0, 1, size=(number_samples, 1))
             grid_values = map_func(values, grid2value_mapping, target_width, target_height)
-            file_name = f'rgc_rf_gridmap_{grid_generate_method}_plot_{i}.png'
-            plot_map_and_save(grid_values, plot_save_folder, file_name=file_name)
+
+            # Create the figure and render the plot in memory
+            fig, ax = plt.subplots(figsize=(8, 8))
+            canvas = FigureCanvas(fig)  # Use canvas to render the plot to an image
+
+            # Plot the data
+            cax = ax.imshow(np.rot90(grid_values, k=1), cmap='viridis', vmin=min_video_value, vmax=max_video_value)
+            fig.colorbar(cax, ax=ax, label="Value")
+            ax.set_title(f"Frame {i}")
+
+            # Draw the canvas and convert to an image
+            canvas.draw()
+            img = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')
+            img = img.reshape(canvas.get_width_height()[::-1] + (3,))
+
+            # Resize the image to fit video dimensions
+            img = cv2.resize(img, (frame_width, frame_height))
+
+            # Write the frame to the video
+            video_writer.write(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+
+            # Close the figure to free memory
+            plt.close(fig)
+
+        # Release video writer
+        video_writer.release()
             
