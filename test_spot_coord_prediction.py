5b4845b6a3a6db6ac135405866bd9a85033c23ea
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-11-15 07:59:24 -0600

    prob 0.5 train

--- test_spot_coord_prediction.py
+++ test_spot_coord_prediction.py
@@ -11,10 +11,10 @@ from datasets.movingdot import RandomMovingSpotDataset, CNN_LSTM_ObjectLocation,
 sequence_length = 20
 num_epochs = 10
 grid_height, grid_width = 24, 32
-prob_vis = 0.8
+prob_vis = 0.5
 num_samples = 10000  # Number of samples to generate in an epoch (arbitrary choice for demonstration)
 batch_size = 64
-sim_id = '11142406'
+sim_id = '11142405'
 
 plot_save_folder  = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Figures/'
 log_save_folder  = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Logs/'
