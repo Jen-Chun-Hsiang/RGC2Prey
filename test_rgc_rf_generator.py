a66d7986fb19e9b7d3d9746745809a6bd977c81a
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-11-15 12:22:07 -0600

    tau change

--- test_rgc_rf_generator.py
+++ test_rgc_rf_generator.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     num_gauss_example = 1
     temporal_filter_len = 50
     sf_scalar = 0.2
-    tau = 10
+    tau = 5
     is_show_rgc_rf_individual = False
     is_show_movie_frames = False
     is_baseline_subtracted = False
