675d6db82489167dfb76a62fadbf0671199042d2
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-11-20 08:52:47 -0600

    not sure

--- train_preycapture.py
+++ train_preycapture.py
@@ -73,10 +73,10 @@ def parse_args():
 
 def main():
     is_show_rgc_rf_individual = False
-    is_show_rgc_tf = True
-    is_show_movie_frames = True
-    is_show_pathes = True
-    is_show_grids = True
+    is_show_rgc_tf = False
+    is_show_movie_frames = False
+    is_show_pathes = False
+    is_show_grids = False
     bottom_img_folder = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images/cropped/grass/'
     top_img_folder    = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images/cropped/cricket/'
     syn_save_folder  = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images/syn_img/'
