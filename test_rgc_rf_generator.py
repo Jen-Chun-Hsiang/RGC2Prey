a5b01e50f168eb4759b07cc937016864043d49b4
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-11-21 08:34:03 -0600

    movie of 250 points

--- test_rgc_rf_generator.py
+++ test_rgc_rf_generator.py
@@ -23,11 +23,11 @@ if __name__ == "__main__":
     syn_save_folder  = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images/syn_img/'
     rf_params_file = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/SimulationParams.xlsx'
     file_name = 'rgc_rf_position_plot.png'
-    video_id = 111203
+    video_id = 111204
     xlim = (-120, 120)
     ylim = (-90, 90)
     rgc_array_rf_size = (320, 240)
-    target_num_centers = 500
+    target_num_centers = 250 #250
     num_step = 20
     num_gauss_example = 1
     temporal_filter_len = 50
