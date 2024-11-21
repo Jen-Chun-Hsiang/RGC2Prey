5225c701c5d42a882e3c7ad23ebec776fd85d194
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-11-21 10:20:36 -0600

    125-15

--- test_rgc_rf_generator.py
+++ test_rgc_rf_generator.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     rf_params_file = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/SimulationParams.xlsx'
     file_name = 'rgc_rf_position_plot.png'
     video_id = 111205
-    grid_id = '125-12'
+    grid_id = '125-15'
     xlim = (-120, 120)
     ylim = (-90, 90)
     rgc_array_rf_size = (320, 240)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     num_gauss_example = 1
     temporal_filter_len = 50
     sf_scalar = 0.2
-    tau = 12 #6, 8.485, 12
+    tau = 15 #6, 8.485, 12
     is_show_rgc_rf_individual = True
     is_show_movie_frames = True
     is_baseline_subtracted = False
