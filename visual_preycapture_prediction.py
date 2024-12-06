cef33e54191d57d21b5a4848dd276902e7db75d3
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-12-06 10:40:59 -0600

    120405-200

--- visual_preycapture_prediction.py
+++ visual_preycapture_prediction.py
@@ -12,8 +12,8 @@ from utils.data_handling import CheckpointLoader
 
 
 def main():
-    experiment_name = 1204202401
-    epoch_number = 140
+    experiment_name = 1204202405
+    epoch_number = 200
     num_display = 6
     checkpoint_path = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/Results/CheckPoints/'
     bottom_img_folder = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images/cropped/grass/'
