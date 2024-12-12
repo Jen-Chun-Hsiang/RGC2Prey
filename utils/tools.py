no permute on movie, correct fixed scaling

utils/tools.py
@@ -64,7 +64,8 @@ class MovieGenerator:
         self.bls_tag = bls_tag
         self.grid_generate_method = grid_generate_method
 
-    def create_figure(self, syn_movie_frame, rgc_output, path_history, path_predict_history, path_bg_history, coord_x_history, coord_y_history, scaling_history):
+    def create_figure(self, syn_movie_frame, rgc_output, path_history, path_predict_history, path_bg_history, coord_x_history, 
+                      coord_y_history, scaling_history, y_min, y_max):
         """
         Create the figure layout with subplots for the movie frame.
 
@@ -100,6 +101,8 @@ class MovieGenerator:
         ax3.plot(path_history[:, 0], path_history[:, 1], label='Ground Truth Path', color='blue')
         ax3.plot(path_predict_history[:, 0], path_predict_history[:, 1], label='Predicted Path', color='orange')
         ax3.legend()
+        ax3.set_ylim(y_min, y_max)
+        ax3.set_xlim(y_min, y_max)
 
         # Coordinate X Subplot
         ax4 = fig.add_subplot(gs[2:3, 3:5])
@@ -108,6 +111,7 @@ class MovieGenerator:
         ax4.plot(path_bg_history[:, 0], label='Background Path', color='green')
         ax4.plot(path_predict_history[:, 0], label='Predicted Path', color='orange')
         ax4.legend()
+        ax4.set_ylim(y_min, y_max)
 
         # Coordinate Y Subplot
         ax5 = fig.add_subplot(gs[3:4, 3:5])
@@ -116,11 +120,13 @@ class MovieGenerator:
         ax5.plot(path_bg_history[:, 1], label='Background Path', color='green')
         ax5.plot(path_predict_history[:, 1], label='Predicted Path', color='orange')
         ax5.legend()
+        ax5.set_ylim(y_min, y_max)
 
         # Scaling Factor Subplot
         ax6 = fig.add_subplot(gs[2:4, 5:6])
         ax6.set_title("Scaling")
         ax6.plot(scaling_history, color='purple')
+        ax6.set_ylim(0, 2.1)
 
         plt.tight_layout()
 
@@ -162,6 +168,9 @@ class MovieGenerator:
         fourcc = cv2.VideoWriter_fourcc(*"mp4v")
         video_writer = cv2.VideoWriter(output_filename, fourcc, self.fps, (self.frame_width, self.frame_height))
 
+        all_y_values = np.concatenate((path, path_bg, path_predict), axis=0)
+        y_min, y_max = np.min(all_y_values), np.max(all_y_values)
+
         path_history = []
         path_predict_history = []
         path_bg_history = []
@@ -186,7 +195,9 @@ class MovieGenerator:
                 path_bg_history=np.array(path_bg_history),
                 coord_x_history=coord_x_history,
                 coord_y_history=coord_y_history,
-                scaling_history=scaling_history
+                scaling_history=scaling_history, 
+                y_min = y_min,
+                y_max = y_max,
             )
 
             # Resize the image to fit video dimensions