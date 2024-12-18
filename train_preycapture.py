849e87d0464dc0141c5e35443a53e88564e4404c
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-12-18 07:54:40 -0600

    correct off rf inputs

--- train_preycapture.py
+++ train_preycapture.py
@@ -185,6 +185,8 @@ def main():
             is_rf_median_subtract=args.is_rf_median_subtract
         )
         multi_opt_sf_off, tf_off, grid2value_mapping_off, map_func_off = rgc_array.get_results()
+    else:
+        multi_opt_sf_off, tf_off, grid2value_mapping_off, map_func_off = None, None, None, None, None
 
     # Check results of RGC array synthesis
     if is_show_rgc_rf_individual:
