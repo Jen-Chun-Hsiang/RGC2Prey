850711b7e317d3bf347f06f11b31923d8e43a359
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2025-01-27 13:19:22 -0600

    get dimension print

--- datasets/sim_cricket.py
+++ datasets/sim_cricket.py
@@ -359,10 +359,12 @@ class Cricket2RGCs(Dataset):
         syn_movie, path, path_bg, scaling_factors = self.movie_generator.generate()
         logging.info( f"   subprocessing...6.1")
         logging.info( f"    syn_movie size {syn_movie.shape}")
+        logging.info( f"    tf size {tf.shape}")
         
         if self.is_direct_image:
             sf_frame = torch.einsum('whn,thw->nt', self.multi_opt_sf, syn_movie)
-            sf_frame = sf_frame.unsqueeze(0) 
+            syn_movie.permute(0, 2, 3, 1)
+            sf_frame = syn_movie.permute(1, 2, 0).unsqueeze(0) 
             tf = np.repeat(self.tf, sf_frame.shape[1], axis=0)
             rgc_time = F.conv1d(sf_frame, tf, stride=1, padding=0, groups=sf_frame.shape[1]).squeeze()
             grid_values_sequence = self.map_func(
