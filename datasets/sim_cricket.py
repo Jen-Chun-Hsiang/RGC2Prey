d4a20542bb88c04185c6530f4b6aec249f4e226a
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2025-02-03 12:39:47 -0600

    debug grid_coords

--- datasets/sim_cricket.py
+++ datasets/sim_cricket.py
@@ -317,7 +317,7 @@ class Cricket2RGCs(Dataset):
                  multi_opt_sf_off=None, tf_off=None, map_func_off=None, grid2value_mapping_off=None, 
                  is_both_ON_OFF=False, quantize_scale = 1, add_noise=False, rgc_noise_std=0.0, smooth_data=False, 
                  smooth_kernel_size=20, sampleing_rate=100, smooth_sigma=0.05, is_rectified=True, is_direct_image=False,
-                grid_coords=None):
+                 grid_coords=None):
         self.num_samples = num_samples
         self.multi_opt_sf = torch.from_numpy(multi_opt_sf).float()
         self.tf = torch.from_numpy(tf.copy()).float().view(1, 1, -1)
@@ -351,8 +351,7 @@ class Cricket2RGCs(Dataset):
         self.smooth_sigma = smooth_sigma
         self.is_rectified = is_rectified
         self.is_direct_image = is_direct_image
-        if grid_coords is not None:
-            self.grid_coords = torch.tensor(grid_coords, dtype=torch.float32)
+        self.grid_coords = torch.tensor(grid_coords, dtype=torch.float32) if grid_coords is not None else None
 
 
     def __len__(self):
