8d0699b4d5823e27bd41846f11dbc053d868ce7c
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2025-02-04 13:08:51 -0600

    test data in different folder

--- visual_preycapture_prediction.py
+++ visual_preycapture_prediction.py
@@ -20,6 +20,8 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Script for Model Training to get 3D RF in simulation")
     parser.add_argument('--experiment_names', type=str, nargs='+', required=True, help="List of experiment names")
     parser.add_argument('--noise_levels', type=float, nargs='+', default=None, help="List of noise levels as numbers")
+    parser.add_argument('--test_bg_folder', type=str, default=None, help="Background folder for testing")
+    parser.add_argument('--test_ob_folder', type=str, default=None, help="Object folder for testing")
 
     return parser.parse_args()
 
@@ -31,10 +33,10 @@ def main():
     for experiment_name in args.experiment_names:
         noise_levels = args.noise_levels if args.noise_levels else [None]
         for noise_level in noise_levels:
-            run_experiment(experiment_name, noise_level)
+            run_experiment(experiment_name, noise_level, args.test_bg_folder, args.test_ob_folder)
 
 
-def run_experiment(experiment_name, noise_level=None):
+def run_experiment(experiment_name, noise_level=None, test_bg_folder=None, test_ob_folder=None):
     epoch_number = 200
     num_display = 3
     frame_width = 640
@@ -44,7 +46,7 @@ def run_experiment(experiment_name, noise_level=None):
     is_making_video = True
     is_add_noise = False
     checkpoint_path = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/Results/CheckPoints/'
-    top_img_folder    = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images/cropped/cricket/'
+    
     rf_params_file = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/SimulationParams.xlsx'
     test_save_folder = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/Results/Figures/'
     coord_mat_file = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/selected_points_summary.mat'
@@ -52,24 +54,32 @@ def run_experiment(experiment_name, noise_level=None):
     mat_save_folder = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/Results/Mats/'
     log_save_folder  = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/Results/Prints/'
 
+    if test_ob_folder is None:
+        test_ob_folder = 'cricket'
+
     if noise_level is not None:
-        file_name = f'{experiment_name}_noise{noise_level}_cricket_location_prediction'
+        file_name = f'{experiment_name}_{test_ob_folder}_noise{noise_level}_cricket_location_prediction'
         is_add_noise = True
     else:
-        file_name = f'{experiment_name}_cricket_location_prediction'
+        file_name = f'{experiment_name}_{test_ob_folder}_cricket_location_prediction'
     initialize_logging(log_save_folder=log_save_folder, experiment_name=file_name)
 
     logging.info( f"{file_name} processing...-1 noise:{noise_level} type:{type(noise_level)}")
     
     # Load checkpoint
     if experiment_name.startswith('1229'):
-        checkpoint_filename = os.path.join(checkpoint_path, f'{experiment_name}_cricket_location_prediction_checkpoint_epoch_{epoch_number}.pth')
+        checkpoint_filename = os.path.join(checkpoint_path, f'{experiment_name}_{test_ob_folder}_cricket_location_prediction_checkpoint_epoch_{epoch_number}.pth')
     else:
-        checkpoint_filename = os.path.join(checkpoint_path, f'{experiment_name}_checkpoint_epoch_{epoch_number}.pth')
+        checkpoint_filename = os.path.join(checkpoint_path, f'{experiment_name}_{test_ob_folder}_checkpoint_epoch_{epoch_number}.pth')
     checkpoint_loader = CheckpointLoader(checkpoint_filename)
     args = checkpoint_loader.load_args()
     training_losses = checkpoint_loader.load_training_losses()
-    bottom_img_folder = f'/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images/cropped/{args.bg_folder}/'  #grass
+
+    
+    if test_bg_folder is None:
+        test_bg_folder = args.bg_folder
+    top_img_folder    = f'/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images/cropped/{test_ob_folder}/'
+    bottom_img_folder = f'/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/CricketDataset/Images/cropped/{test_bg_folder}/'  #grass
 
     logging.info( f"{file_name} processing...0")
     if args.is_GPU:
@@ -111,7 +121,7 @@ def run_experiment(experiment_name, noise_level=None):
     logging.info(f"Maximum value: {np.amax(grid_centers)}")
     logging.info(f"Maximum value: {np.amin(grid_centers)}")
 
-    raise ValueError(f"Temporal close exam processing...")
+    # raise ValueError(f"Temporal close exam processing...")
     logging.info( f"{file_name} processing...2")
 
     movie_generator = SynMovieGenerator(top_img_folder, bottom_img_folder,
