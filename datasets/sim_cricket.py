e2d44167598e3e4a5a670b51c0c1d04400e42ebb
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2025-02-05 20:59:00 -0600

    print tf

--- datasets/sim_cricket.py
+++ datasets/sim_cricket.py
@@ -381,8 +381,8 @@ class Cricket2RGCs(Dataset):
             # print(f'multi_opt_sf_off shape: {self.multi_opt_sf_off.shape}')
             tf = np.repeat(self.tf, self.multi_opt_sf.shape[1], axis=0)
             tf_off = np.repeat(-self.tf_off, self.multi_opt_sf_off.shape[1], axis=0)
-            # print(f'tf shape: {self.tf.shape}')
-            # print(f'tf_off shape: {self.tf_off.shape}')
+            print(f'tf shape: {self.tf.shape}')
+            print(f'tf_off shape: {self.tf_off.shape}')
             # tf = torch.tensor(np.repeat(self.tf, self.multi_opt_sf.shape[1], axis=0), dtype=torch.float32)
             # tf_off = torch.tensor(np.repeat(-self.tf_off, self.multi_opt_sf_off.shape[1], axis=0), dtype=torch.float32)
             tfs = [tf, tf_off]
