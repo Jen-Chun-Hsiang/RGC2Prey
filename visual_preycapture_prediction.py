d1c75128d082d7dbd7846ad0bf3d71e64525747a
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2025-02-21 12:27:58 -0600

    print more

--- visual_preycapture_prediction.py
+++ visual_preycapture_prediction.py
@@ -149,6 +149,8 @@ def run_experiment(experiment_name, noise_level=None, test_bg_folder=None, test_
         multi_opt_sf_off, tf_off, grid2value_mapping_off, map_func_off, rgc_locs_off = None, None, None, None, None
 
     if is_show_rgc_grid:
+        print(f'grid_centers: {grid_centers}')
+        print(f'rgc_locs_off: {rgc_locs_off}')
         plot_coordinate_and_save(grid_centers, rgc_locs_off, plot_save_folder, file_name=f'{args.experiment_name}_rgc_grids_test.png')
 
     # raise ValueError(f"Temporal close exam processing...")
