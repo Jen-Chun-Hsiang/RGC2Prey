fc97412c4382efdef4fcfb85a9b287bfd6db1a1f
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2025-02-23 18:11:35 -0600

    visualize with is_channel_normalization

--- visual_preycapture_prediction.py
+++ visual_preycapture_prediction.py
@@ -121,6 +121,8 @@ def run_experiment(experiment_name, noise_level=None, test_bg_folder=None, test_
         args.bg_processing_type = 'one-proj'
     if not hasattr(args, 'grid_noise_level'):
         args.grid_noise_level = 0.3
+    if not hasattr(args, 'is_channel_normalization'):
+        args.is_channel_normalization = False
 
     process_seed(args.seed)
 
@@ -180,7 +182,8 @@ def run_experiment(experiment_name, noise_level=None, test_bg_folder=None, test_
                                      lstm_num_layers=args.lstm_num_layers, output_dim=args.output_dim,
                                     input_height=grid_width, input_width=grid_height, conv_out_channels=args.conv_out_channels,
                                     is_input_norm=args.is_input_norm, is_seq_reshape=args.is_seq_reshape, CNNextractor_version=args.cnn_extractor_version,
-                                    num_input_channel=num_input_channel, bg_info_cost_ratio=args.bg_info_cost_ratio, bg_processing_type=args.bg_processing_type)
+                                    num_input_channel=num_input_channel, bg_info_cost_ratio=args.bg_info_cost_ratio, bg_processing_type=args.bg_processing_type,
+                                    is_channel_normalization=args.is_channel_normalization)
     optimizer = torch.optim.Adam(model.parameters())
     model, optimizer, _ = checkpoint_loader.load_checkpoint(model, optimizer)
     criterion = nn.MSELoss()
