da152d640fcf0fda2c8949d5047002371129abac
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2025-02-25 09:22:03 -0600

    test size of syn_movie

--- datasets/sim_cricket.py
+++ datasets/sim_cricket.py
@@ -597,16 +597,15 @@ class SynMovieGenerator:
             top_img_disparity_positions = path.copy()
             top_img_disparity_positions[:, 0] += disparity
             bounds = (-self.boundary_size[0]/2, self.boundary_size[0]/2, -self.boundary_size[1]/2, self.boundary_size[1]/2)
-            plot_save_folder =  '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/Results/Figures/temps/'
             top_img_positions_shifted, top_img_disparity_positions_shifted = adjust_trajectories(bounds, path.copy(), top_img_disparity_positions)
+            # plot_save_folder =  '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RGC2Prey/Results/Figures/temps/'
             # plot_trajectories(bounds, top_img_positions, top_img_disparity_positions, top_img_positions_shifted, 
             #                  top_img_disparity_positions_shifted, plot_save_folder, filename="trajectory_plot.png")
             syn_movie = synthesize_image_with_params_batch(
                 bottom_img_path, top_img_path, top_img_positions_shifted, bottom_img_positions,
                 scaling_factors, self.crop_size, alpha=1.0, top_img_positions_shifted=top_img_disparity_positions_shifted
             )
-            print(f'syn_movie size: {syn_movie.shape}')
-            raise ValueError(f"check disparity ...")
+            
 
         else:
             # Generate the batch of images
@@ -614,6 +613,9 @@ class SynMovieGenerator:
                 bottom_img_path, top_img_path, top_img_positions, bottom_img_positions,
                 scaling_factors, self.crop_size, alpha=1.0
             )
+        
+        print(f'syn_movie size: {syn_movie.shape}')
+        raise ValueError(f"check disparity ...")
         
         # Correct for the cricket head position
         bg_image_name = get_filename_without_extension(bottom_img_path)
