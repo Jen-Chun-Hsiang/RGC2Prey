d71e16089f2fc9ed994e6b5506d8b8173caf3ae7
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2025-05-21 10:24:19 -0500

    update training and visualization step with contrast control

--- visual_preycapture_prediction.py
+++ visual_preycapture_prediction.py
@@ -141,6 +141,12 @@ def run_experiment(experiment_name, noise_level=None, test_bg_folder=None, test_
             args.rectified_thr_OFF = args.rectifed_thr_OFF
         else:
             args.rectified_thr_OFF = 0.0
+    if not hasattr(args, 'bottom_contrast'):
+        args.bottom_contrast = 1.0
+    if not hasattr(args, 'top_contrast'):
+        args.top_contrast = 1.0
+    if not hasattr(args, 'mean_diff_offset'):
+        args.mean_diff_offset = 0.0
             
 
     process_seed(args.seed)
@@ -191,7 +197,7 @@ def run_experiment(experiment_name, noise_level=None, test_bg_folder=None, test_
         velocity_randomness_bg=args.velocity_randomness_bg, angle_range_ob=args.angle_range_ob, angle_range_bg=args.angle_range_bg, 
         coord_mat_file=coord_mat_file, correction_direction=args.coord_adj_dir, is_reverse_xy=args.is_reverse_xy, 
         start_scaling=args.start_scaling, end_scaling=args.end_scaling, dynamic_scaling=args.dynamic_scaling, is_binocular=args.is_binocular,
-        interocular_dist=args.interocular_dist
+        interocular_dist=args.interocular_dist, bottom_contrast=1.0, top_contrast=1.0, mean_diff_offset=0.0
     )
 
     logging.info( f"{file_name} processing...3")
