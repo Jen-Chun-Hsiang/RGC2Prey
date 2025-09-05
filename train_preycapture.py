113530af0b0e533ce8c476771f8de727b14d64b8
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2025-09-05 11:41:06 -0500

    Remove commented-out sys.exit() call in main function for cleaner code

--- train_preycapture.py
+++ train_preycapture.py
@@ -421,7 +421,7 @@ def main():
         print(f'iteration: {i}')
         sequence, path, path_bg, syn_movie, scaling_factors, _, _, _ = train_dataset[i]
 
-    # sys.exit(0)
+    sys.exit(0)
     if is_show_movie_frames:
         for i in range(syn_movie.shape[0]):
             Timg = syn_movie[i, :, :]
