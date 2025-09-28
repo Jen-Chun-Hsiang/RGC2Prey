aec3cdfa678d4e6be875e77003afb50320428d69
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2025-09-27 20:39:48 -0500

    Ensure deterministic behavior by enforcing use_deterministic_algorithms in set_seed function

--- utils/initialization.py
+++ utils/initialization.py
@@ -22,6 +22,7 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.use_deterministic_algorithms(True)  # Ensure deterministic behavior
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
